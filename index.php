0950bc19acfa99162c1a6a0b6552b62c5f59051b
Andrey Smelov <xor29a@bk.ru>
2016-02-22 19:33:33 +0300

    Simple error handler and config

--- index.php
+++ index.php
@@ -1,10 +1,27 @@
 <?php
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+ini_set('display_errors', 0);
+error_reporting(-1);
 
+// Composer
 require 'vendor/autoload.php';
 
-$app = new \Slim\App();
+// Try to get config
+if (is_file($file = __DIR__ . '/config.php')) {
+	$config = require $file;
+} else {
+	$config = ['env' => 'prod'];
+}
+
+// Check environment
+if (!isset($config['env']) || !in_array($config['env'], ['dev', 'test', 'prod'])) {
+	die('Please set env to one of (dev,test,prod) in file: ' . $file);
+}
+
+$app = new \Slim\App(['config' => $config]);
+
+// Set error handler
+$container = $app->getContainer();
+$container['cmsErrorHandler'] = new \xorik\cms\ErrorHandler($container);
+
 $app->run();
