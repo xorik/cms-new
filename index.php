2fdd5a3ad88c413863bbdb733323524aa17b0f56
Andrey Smelov <xor29a@bk.ru>
2016-02-22 20:08:14 +0300

    Default config, which can be replaced with config.php

--- index.php
+++ index.php
@@ -3,15 +3,23 @@
 ini_set('display_errors', 0);
 error_reporting(-1);
 
-// Composer
-require 'vendor/autoload.php';
-
 // Try to get config
 if (is_file($file = __DIR__ . '/config.php')) {
 	$config = require $file;
-} else {
-	$config = ['env' => 'prod'];
 }
+
+$default_config = [
+	'env' => 'prod',
+	'vendorDir' => __DIR__ . '/vendor/',
+	'configDir' => __DIR__ . '/config/',
+	'cacheDir' => __DIR__ . '/cache/'
+];
+
+// Merge default config and config
+$config = isset($config) ? array_replace($default_config, $config) : $default_config;
+
+// Composer
+require $config['vendorDir'] . 'autoload.php';
 
 // Check environment
 if (!isset($config['env']) || !in_array($config['env'], ['dev', 'test', 'prod'])) {
